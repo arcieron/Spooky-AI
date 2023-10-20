21ca4059255817e349578a9fc0689bcd038300ed
Shadeer Sadikeen <shadeersadikeen@gmail.com>
2023-10-21 00:20:51 +0530

    changed the iamge moddal

--- components/ImageList.tsx
+++ components/ImageList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ImageModal } from "./ImageModal";
+import { ImageModal } from "@/components/ImageModal";
 import { Button } from "@/components/ui/button"
 import {
     Dialog,
