4f0df58880e5b94946bc8b44aef6a0ff0064ec25
Shadeer Sadikeen <shadeersadikeen@gmail.com>
2023-10-20 20:00:17 +0530

    added the popup

--- components/ImageList.tsx
+++ components/ImageList.tsx
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { ImageModal } from "./ImageModal";
+import { Button } from "@/components/ui/button"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 
-const ImageList = ({images}: any) => {
+const ImageList = ({ images }: any) => {
     // Check if the "images" array is empty
     if (images?.length === 0) {
         return <p>No images generated yet.</p>;
     }
+
+    const [selectedImage, setSelectedImage] = useState<string>("");
+
+    const handleImageClick = (imageUrl: string) => {
+        setSelectedImage(imageUrl);
+    };
 
     // Function to handle the image download
     const handleDownload = async (imageUrl: any) => {
@@ -18,32 +37,62 @@ const ImageList = ({images}: any) => {
         link.click()
         document.body.removeChild(link)
     };
+
     const reversedImages = [...images].reverse();
 
     // If there are images, map over the array and render each image with a download button
     return (
-        <div className="w-full grid gap-2 grid-cols-2 mt-4 min-h-40">
-            {reversedImages.map((imageUrl, index) => (
-                <div key={index} className="relative  rounded-lg  shadow-2xl h-full">
-                    <img
-                        src={imageUrl}
-                        alt={`Generated Image ${index + 1}`}
-                        className="h-full object-cover object-left-top rounded-lg w-full"
-                    />
+        <Dialog>
+            <div className="w-full grid gap-2 grid-cols-2 mt-4 min-h-40">
+                {reversedImages.map((imageUrl, index) => (
+                    <DialogTrigger asChild>
+                        <div onClick={() => handleImageClick(imageUrl)}>
+                            <div key={index} className="relative  rounded-lg  shadow-2xl h-full">
+                                <img
+                                    src={imageUrl}
+                                    alt={`Generated Image ${index + 1}`}
+                                    className="h-full object-cover object-left-top rounded-lg w-full"
+                                />
 
-                    <div
-                        className="absolute bottom-0 rounded-b-lg left-0 right-0 flex items-end  justify-center  h-2/5 w-full bg-gradient-to-t from-white via-transparent to-transparent">
-                        <button className=" mb-8  rounded-lg bg-black  px-8  py-2 text-white shadow-md"
-                                onClick={() => handleDownload(imageUrl)}
-                        >
-                            Download
+                                <div
+                                    className="absolute bottom-0 rounded-b-lg left-0 right-0 flex items-end  justify-center  h-2/5 w-full bg-gradient-to-t from-white via-transparent to-transparent">
+                                    <button className=" mb-8  rounded-lg bg-black  px-8  py-2 text-white shadow-md"
+                                        onClick={() => handleDownload(imageUrl)}
+                                    >
+                                        Download
+                                    </button>
+                                </div>
 
-                        </button>
+                            </div>
+                        </div>
+                    </DialogTrigger>
+
+                ))}
+            </div>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>Selected Image</DialogTitle>
+                    <DialogDescription>
+                        Download and share your image!
+                    </DialogDescription>
+                </DialogHeader>
+
+                
+                <DialogFooter className="w-full flex ">
+                    <div className=" justify-center grid items-center ">
+                    {selectedImage && (
+                        <img src={selectedImage} alt="Selected" className="w-full mb-4 object-contain h-60 rounded-2xl" />
+                    )}
+                        <button className="   rounded-lg bg-black  px-8  py-2 text-white shadow-md"
+                                        onClick={() => handleDownload(selectedImage)}
+                                    >
+                                        Download
+                                    </button>
                     </div>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
 
-                </div>
-            ))}
-        </div>
     );
 };
 
--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -77,7 +77,7 @@ const DialogFooter = ({
 }: React.HTMLAttributes<HTMLDivElement>) => (
   <div
     className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
+      "flex sm:flex-row justify-center  sm:space-x-2",
       className
     )}
     {...props}
